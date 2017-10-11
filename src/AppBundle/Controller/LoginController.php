e75c17c8c2131b6cb04d68cc406699d935e01f22
Pravin <p.kudale@easternenterprise.com>
2017-10-11 20:34:11 +0530

    added template

--- src/AppBundle/Controller/LoginController.php
+++ src/AppBundle/Controller/LoginController.php
@@ -4,16 +4,33 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class LoginController
+class LoginController extends \Symfony\Component\HttpKernel\Tests\Controller
 {
     /**
-     * @Route("/login/index")
+     * @Route("/login", name="login_form")
      */
     public function indexAction()
     {
+//        return new Response(
+//            '<html><body>Login</body></html>'
+//        );
+         $this->render('default/index.html.twig');
+    }
+    
+    /**
+     * 
+     * @Route(
+     *      "/list{page}", 
+     *      name="user_list", 
+     *      requirements={"page":"\d+"}
+     * )
+     */
+    public function listAction($page = 1)
+    {
         return new Response(
-            '<html><body>Login</body></html>'
+            '<html><body>user list</body></html>'
         );
     }
 }
